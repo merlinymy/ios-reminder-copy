0836649e9735a1f849a1e68994fe19d1d8322898
merlinymy <lordhorcrux@gmail.com>
2025-01-16 11:17:39 -0800

    add color picking logic

--- src/components/newList.js
+++ src/components/newList.js
@@ -1,27 +1,69 @@
+import { iconList, colorList } from "../util";
+
 export default (function newList() {
+    // TODO: add touch ctrl
     const wrapper = document.createElement("div");
     wrapper.classList.add('list-wrap');
     const content = 
     `
     <div class="new-list-card">
-    <div class="top-tool">
+    <div class="top-tool space-between">
         <button class="cancel">Cancel</button>
         <span>New List</span>
         <button class="done">Done</button>
     </div>
-    <div class="init">
+    <div class="init card-wrap">
         <div class="big-icon">
-
+            <span class="material-symbols-outlined">
+                list
+            </span>
         </div>
         <input type="text" placeholder="List Name">
     </div>
-    <div class="color"></div>
-    <div class="icons"></div>
+    <div class="wrap-center">
+        <div class="color-wrap card-wrap"></div>
+    </div>
+    
+    <div class="icons-wrap card-wrap"></div>
 </div>
     `
     wrapper.innerHTML = content;
     // wrapper.style.height = `0vh`;
     wrapper.style.transform = `translateY(10%)`;
+
+    const colorDiv = wrapper.querySelector('.color-wrap');
+    let selectColor;
+    colorList.forEach((color) => {
+        const colorSpanWrap = document.createElement('span');
+        colorSpanWrap.classList.add('color-span-wrap', 'circle-not-select');
+
+        const colorSpan = document.createElement('span');
+        colorSpan.classList.add('color','circle',`${color}`);
+        colorSpan.style.backgroundColor = `${color}`;
+
+
+        colorSpan.addEventListener('click', (event)=>{
+            selectColor = color;
+            event.target.parentElement.classList.add('circle-select');
+            event.target.parentElement.classList.remove('circle-not-select');
+            event.target.parentElement.parentElement.childNodes.forEach((child) => {
+                console.log(child.firstChild.classList.contains(color))
+                if (child.firstChild.classList.contains(color)) {
+                    child.classList.remove('circle-not-select');
+                    child.classList.add('circle-select');
+                } else {
+                    child.classList.add('circle-not-select');
+                    child.classList.remove('circle-select');
+                }
+            })
+        })
+
+        colorSpanWrap.append(colorSpan);
+        colorDiv.appendChild(colorSpanWrap);
+    })
+
+
+
     
     wrapper.querySelector('button.cancel').addEventListener('click', (e)=>{
         e.preventDefault();
@@ -33,7 +75,9 @@ export default (function newList() {
         };
         setTimeout(removeWrapper, 300);
         // wrapper.addEventListener('transitionend', removeWrapper);
-    })
+    });
+
+
     
     return wrapper;
 })();
---
+++ src/util.js
@@ -0,0 +1,15 @@
+const iconList = [
+    'list', 'bookmark', 'keep', 'featured_seasonal_and_gifts', 'cake',
+    'school', 'personal_bag', 'design_services', 'description', 'import_contacts',
+    'wallet', 'credit_card', 'payments', 'exercise', 'restaurant', 'wine_bar',
+    'pill', 'stethoscope', 'weekend', 'house', 'apartment', 'account_balance', 
+    'account_balance', 'tv', 'music_note', 'shopping_cart', 'shopping_basket',
+    'diversity_3', 'bath_private', 'directions_car', 'work', 'content_cut',
+]
+
+const colorList = [
+    'red', 'orange', 'gold', 'green', 'cyan', 'blue', 'purple', 
+    'indianred', 'violet', 'rosybrown', 'grey', 'salmon'
+]
+
+export {iconList, colorList};
