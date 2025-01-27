1e63b2edbadb380cf5d7329a034225a142b3bb4e
merlinymy <lordhorcrux@gmail.com>
2025-01-27 14:08:06 -0600

    add ui to details page

--- src/components/detailsPage.js
+++ src/components/detailsPage.js
@@ -3,9 +3,17 @@ import detailPageStruct from './detailsPage.html';
 export const createDetails = function() {
     const body = document.querySelector('body');
     const newReminderPage = document.querySelector('.new-reminder-div');
+    const newReminderTitle = newReminderPage.querySelector('textarea#title');
     const component = document.createElement('div');
     component.classList.add('details-wrap')
     component.innerHTML = detailPageStruct;
+    const addBtn = component.querySelector('button.done');
+    if (newReminderTitle.value) {
+        addBtn.disabled = false;
+    } else {
+        addBtn.disabled = true;
+
+    }
 
     const backBtn = component.querySelector('button.cancel');
     backBtn.addEventListener('click', (event) => {
